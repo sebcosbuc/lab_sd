a4d5d2dfb9e52b6e019a5ecf1414cfe754a537bb
sebcosbuc <145281065+sebcosbuc@users.noreply.github.com>
2023-10-28 18:21:22 +0300

    Update lista1.cpp

--- lista1.cpp
+++ lista1.cpp
@@ -126,6 +126,7 @@ void adaugare_pe_pozitie_data()
         {
             if(i==pozitie-1)
             {
+                cout<<"i="<<i;
                 aux=new nod;
                 aux->info=informatie;
                 aux->next=curent->next;
@@ -149,8 +150,7 @@ void adaugare_dupa_valoare_data()
 {
     int informatie,valoare;
     cout<<"\n --------\n";
-    cout<<"\nadaugare pe pozitie data \n\n";
-    cout<<"pozitia pe care vreti sa inserati informatia";
+    cout<<"valoarea dupa care vreti sa inserati informatia";
     cin>>valoare;
     cout<<"\n ce informatie doriti sa adaugati? ";
     cin>>informatie;
@@ -162,10 +162,10 @@ void adaugare_dupa_valoare_data()
     {
         nod *aux, *curent;
         curent=primul;
-        int i=1, ok=0;
+        int ok=0;
         while(curent)
         {
-            if(i==valoare)
+            if(curent->info==valoare)
             {
                 aux=new nod;
                 aux->info=informatie;
@@ -176,7 +176,6 @@ void adaugare_dupa_valoare_data()
             }
             else
             {
-                i++;
                 curent=curent->next;
             }
         }
@@ -186,26 +185,172 @@ void adaugare_dupa_valoare_data()
     }
 }
 
+void stergere_primul_nod()
+{
+    if(primul==NULL)
+        cout<<"lista nu exista. nu pot sterge";
+    else
+    {
+        cout<<"\nsterg primul nod din lista\n";
+        nod *aux;
+        aux=primul;
+        primul=primul->next;
+        delete aux;
+    }
+}
+
+void stergere_ultimul_nod()
+{
+    if(primul==NULL)
+        cout<<"lista nu exista. nu pot sterge";
+    else
+    {
+        if(primul==ultimul)
+        {
+            delete primul;
+            primul=ultimul=NULL;
+        }
+        else
+        {
+            nod *curent;
+            curent=primul;
+            while(curent->next!=ultimul)
+                curent=curent->next;
+            delete curent->next;
+            curent->next=NULL;
+            ultimul=curent;
+        }
+    }
+}
+
+void stergere_nod_de_pe_pozitia_data()
+{
+    int i,pozitie,ok;
+    nod *aux;
+    cout<<"dati pozitia de pe care sa se stearga nodul: ";
+    cin>>pozitie;
+    ok=0;i=1;
+    if(primul==NULL)
+        cout<<"lista nu exista. nu pot sterge";
+    else
+    {
+        nod *curent;
+        curent=primul;
+        if(pozitie==1)
+            stergere_primul_nod();
+        else
+        {
+            while (curent)
+                if(pozitie-1!=i)
+            {
+                curent=curent->next;
+                i++;
+            }
+            else
+                if(curent!=ultimul)
+            {
+                aux=curent->next;
+                curent->next=curent->next->next;
+                delete aux;
+                ok=1;
+                break;
+            }
+            else stergere_ultimul_nod();
+            if(ok==0)
+                cout<<"lista nu are nici un element pe pozitia data";
+        }
+    }
+}
+
+void stergere_nod_cu_valoare_data()
+{
+    int i,valoare,ok;
+    nod *aux;
+    cout<<"ce valoare vreti sa se stearga din lista?";
+    cin>>valoare;
+    ok=0;
+    if(primul==NULL)
+        cout<<"lista nu exista. nu pot sterge";
+    else
+        if(primul->info==valoare)
+            stergere_primul_nod();
+        if(ultimul->info==valoare)
+            stergere_ultimul_nod();
+        else
+        {
+            nod *curent;
+            curent=primul;
+            while(curent)
+                if(curent->next->info!=valoare)
+            {
+                curent=curent->next;
+                cout<<curent->info; getch();
+            }
+            else
+                if(curent->next!=ultimul)
+            {
+                aux=curent->next;
+                curent->next=curent->next->next;
+                delete aux;
+                ok=1;
+                break;
+            }
+            if(ok==0)
+                cout<<"nu exista aceasta valoare in lista";
+        }
+}
+
+void stergere_lista()
+{
+    if(primul==NULL)
+        cout<<"lista nu exista";
+    else
+        while(primul)
+        if(primul!=ultimul)
+            stergere_primul_nod();
+        else
+        {
+            delete primul;
+            delete ultimul;
+            primul=ultimul=NULL;
+        }
+}
+
 int main()
 {
     int n;
     creare_lista();
     afisare_lista(); cout<<"\n";
     adaugare_la_sfarsit();
-    cout<<"\n\n dupa adaugare la sfarsit lista devine";
+    cout<<"\n\n dupa adaugare la sfarsit lista devine\n";
     afisare_lista();
     cout<<"\n";
     adaugare_la_inceput();
-    cout<<"\n\n dupa adaugare la inceput lista devine";
+    cout<<"\n\n dupa adaugare la inceput lista devine\n";
     afisare_lista();
     cout<<"\n";
     adaugare_pe_pozitie_data();
-    cout<<"\n\n dupa adaugare pe pozitia data lista devine";
+    cout<<"\n\n dupa adaugare pe pozitia data lista devine\n";
     afisare_lista();
     cout<<"\n";
     adaugare_dupa_valoare_data();
-    cout<<"\n\n dupa adaugare dupa valoarea data lista devine";
+    cout<<"\n\n dupa adaugare dupa valoarea data lista devine\n";
     afisare_lista();
     cout<<"\n";
+    stergere_primul_nod();
+    cout<<"\n\ndupa stergerea primului nod lista devine:";
+    afisare_lista();cout<<"\n";
+    stergere_ultimul_nod();
+    cout<<"\n\ndupa stergerea ultimului nod lista devine:";
+    afisare_lista();cout<<"\n";
+    stergere_nod_de_pe_pozitia_data();
+    cout<<"\n\ndupa stergerea unui nod cu pozitia data lista devine:";
+    afisare_lista();cout<<"\n";
+    stergere_nod_cu_valoare_data();
+    cout<<"\n\ndupa stergerea unui nod cu valoarea data lista devine:";
+    afisare_lista();cout<<"\n";
+    stergere_lista();
+    cout<<"\n\ndupa stergere lista devine";
+    afisare_lista();cout<<"\n";
     return 0;
 }
