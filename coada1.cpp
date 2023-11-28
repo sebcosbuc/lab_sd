8eb6a473d4f79718e75191b28f2de9b5859c8ad3
sebcosbuc <145281065+sebcosbuc@users.noreply.github.com>
2023-11-28 12:05:15 +0200

    Create coada1.cpp

---
+++ coada1.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <conio.h>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+struct nod
+{
+    int info;
+    nod *next;
+};
+
+nod *l;
+int n;
+
+nod* inserare(int info, nod *l)
+{
+    nod *aux1;
+    if(l==NULL)
+    {
+        l=new nod;
+        l->info=info;
+        l->next=NULL;
+        return l;
+    }
+    else
+        {
+            aux1=new nod;
+            aux1->info=info;
+            nod *aux2;
+            aux2=l;
+            while(l->next!=NULL)
+                l=l->next;
+            l->next=aux1;
+            aux1->next=NULL;
+            return aux2;
+        }
+}
+
+nod* stergere(nod *l)
+{
+    nod *aux1, *aux2;
+    if(l!=NULL)
+    {
+        aux1=l;
+        aux2=l->next;
+        cout<<"\n voi sterge nodul care contine "<<aux1->info<<endl;
+        delete aux1;
+        delete aux2;
+    }
+    else
+    {
+        cout<<"\n coada este goala. Nu mai am ce sa sterg "<<endl;
+        return NULL;
+    }
+}
+
+void creare_coada()
+{
+    nod *a;
+    int info;
+    ifstream f("in.txt");
+    f>>n;
+    cout<<n;
+    for(int i=0;i<n;i++)
+    {
+        f>>info;
+        l=inserare(info,l);
+    }
+    f.close();
+}
+
+void afisare_coada(nod *a)
+{
+    a=l;
+    if(a==NULL)
+        cout<<"coada este vida";
+    else
+    {
+        cout<<"\nelementele cozii sunt: ";
+        while(a)
+        {
+            cout<<a->info<<" ";
+            a=a->next;
+        }
+    }
+}
+int main()
+{
+    creare_coada();
+    afisare_coada(l);
+    return 0;
+}
